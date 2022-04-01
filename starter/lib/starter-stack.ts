76ddd6eb9c885d077b65d209854aba889676e50f
Oliver Wang <ole3021@Olivers-MacBook-Pro.local>
2022-04-01 15:14:27 +0800

    udpate lambda & apigw example

--- starter/lib/starter-stack.ts
+++ starter/lib/starter-stack.ts
@@ -1,10 +1,20 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 
 export class StarterStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // Logic goes here.
+    const hello = new Function(this, 'HelloHandler', {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
+
+    new LambdaRestApi(this, 'Endpoint', {
+      handler: hello
+    });
   }
 }
